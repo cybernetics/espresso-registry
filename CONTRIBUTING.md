96412b00b7c236aa4643fcfe4d498860d5349e64
hlafaille <hlafaille@proton.me>
2024-03-14 22:46:29 -0400

    working

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+# Contributing
+I welcome any and all contributors to this project. This project is licensed under the MIT license, just like Espresso.
+
+## Contribute an Artifact Registration
+Contributing an artifact registration is straightforward. First, create an [Issue](https://github.com/hlafaille/espresso-registry/issues) under the repository with the template "**Artifact Registry Addition**".
--- src/introspect/mod.rs
+++ src/introspect/mod.rs
@@ -1,9 +1,18 @@
+/// Represents a package.
 pub struct Package {
     pub group_id: String,
     pub artifact_id: String,
+    pub source_repository: String,
     pub versions: Vec<String>,
 }
 
+/// Represents the supported package types. This will dictate how they're applied at compile time.
+pub enum Flags {
+    annotation_processor,
+}
+
+/// Represents a specific release/version of the package.
 pub struct PackageVersion {
-    pub 
+    pub version: String,
+    pub types: Vec<Flags>
 }
