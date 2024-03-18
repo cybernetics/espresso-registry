f8e2f6a812a4050b1a9a0f4d40280188ffd1ae45
hlafaille <hlafaille@proton.me>
2024-03-18 13:16:24 -0400

    #8 implmented environment variable configuration for `DynamicAbsolutePaths.registry`

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -1,15 +1,35 @@
 use std::{env, error, result};
 
+use tracing_subscriber::registry;
+
+use crate::util::error::EspressoError;
+
 pub struct DynamicAbsolutePaths {
-    pub registry: String
+    pub registry: String,
 }
 
+/// Get the absolute path to the registry
+///
+/// # Arguments
+/// * `cwd`: Reference to a `String` containing the current working directory
+///
+/// # Returns
+/// Propagated errors, where `Ok` is a `String` containing the absolute path to the registry
+fn get_registry_path(cwd: &String) -> result::Result<String, Box<dyn error::Error>> {
+    let mut registry_path = env::var("ESPRESSO_REGISTRY_PATH")?;
+    if registry_path.is_empty() {
+        registry_path = cwd.clone() + "/registry";
+    }
+    Ok(registry_path)
+}
+
+/// Get a `DynamicAbsolutePaths` struct
+///
+/// # Returns
+/// Propagated errors, where `Ok` is a `DyanmicAbsolutePaths` struct
 pub fn get_dynamic_absolute_paths() -> result::Result<DynamicAbsolutePaths, Box<dyn error::Error>> {
-    let cwd = env::current_dir()?;
-    let mut cwd_string: String = cwd.to_string_lossy().into_owned();
-    Ok(
-        DynamicAbsolutePaths {
-        registry: (cwd_string.clone() + "/registry").to_string()
-        }
-    )
+    let cwd = env::current_dir()?.to_string_lossy().into_owned();
+    Ok(DynamicAbsolutePaths {
+        registry: get_registry_path(&cwd)?,
+    })
 }
